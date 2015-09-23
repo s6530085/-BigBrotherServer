f624259b1f3222effb43e9c2799ac1ffec25c28f
study_sun <msun@gongpingjia.com>
2015-09-23 18:47:06 +0800

    implement args

--- lottery.py
+++ lottery.py
@@ -1,26 +1,31 @@
 # coding=UTF-8
-from flask import Flask
+from flask import Flask,request
 import random
 import json
-
-def geneRandomDistinctList(totalCount, minValue, maxValue):
-    sourceList = []
-    while (len(sourceList) < totalCount) :
-        while (1):
-            r = random.choice(range(minValue,maxValue))
-            if not (r in sourceList):
-                sourceList.append(r)
-                break
-    sourceList.sort()
-    return sourceList
-
-
-def geneDLT():
-    reds = geneRandomDistinctList(5, 1, 35)
-    blues = geneRandomDistinctList(2, 1, 12)
-    return reds + blues
+import SuperLotto
+import BaseAlgorithm
+import string
 
 app = Flask(__name__)
+
+def lotteryAlgorithm(type, algorithm, count):
+    if type == None:
+        type = 'superlotto'
+    if algorithm == None:
+        algorithm = 'random'
+    if count == None:
+        count = 1
+    count = int(count)
+    #目前只支持随机嘻嘻
+
+    if type.lower() == 'SuperLotto'.lower():
+        if algorithm.lower() == 'random':
+            return SuperLotto.random(count)
+        else:
+            return SuperLotto.random(count)
+    else:
+        return SuperLotto.random(count)
+
 
 
 @app.route('/')
@@ -30,9 +35,14 @@ def hello_world():
 
 @app.route('/lottery')
 def lottery():
-    ls = geneDLT()
-    s = map(str, ls)
-    return json.dumps({'status' : 'success', 'lottery_list' : [s]})
+    # 参数优先看 类型(大乐透|福彩)，其次看玩法(随机|真算)，最后看注数，默认值是大乐透，随机，一注
+    type = request.args.get('type')
+    algorithm = request.args.get('algorithm')
+    count = request.args.get('count')
+    ls = lotteryAlgorithm(type, algorithm, count)
+
+    return json.dumps({'status' : 'success', 'lottery_list' : ls})
+
 
 if __name__ == '__main__':
     app.run()
